997a3a40f0058fe1e5b6923333bd43777b95a2b5
Xarianne <games.clinking002@passinbox.com>
2025-06-30 17:57:59 +0100

    Replacing KDE with Hyprland

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,10 +10,10 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux
+# dnf5 install -y tmux
 
 # Enable Hyprland COPR repository
-dnf5 -y copr enable @solopasha/hyprland
+dnf5 -y copr enable @feverfew/hyprland
 
 # Remove KDE components (keeping sddm)
 rpm-ostree override remove \
