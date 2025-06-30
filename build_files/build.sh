9246bda8e8bc3003d9a649671f1487cd2c4464e7
Xarianne <games.clinking002@passinbox.com>
2025-06-30 18:11:27 +0100

    Replacing KDE with Hyprland

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,12 +9,6 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-# dnf5 install -y tmux
-
-# Enable Hyprland COPR repository
-dnf5 -y copr enable @feverfew/hyprland
-
 # Remove KDE components (keeping sddm)
 rpm-ostree override remove \
     plasma-desktop \
@@ -23,9 +17,15 @@ rpm-ostree override remove \
     krunner \
     kwin
 
-# Install Hyprland and its dependencies
-rpm-ostree install \
-    hyprland \
+# Use a COPR Example:
+dnf5 y copr enable solopasha/hyprland
+# dnf5 -y copr enable ublue-os/staging
+# dnf5 -y install package
+# Disable COPRs so they don't end up enabled on the final image:
+# dnf5 -y copr disable ublue-os/staging
+
+# this installs a package from fedora repos
+dnf5 install -y hyprland \
     xdg-desktop-portal-hyprland \
     foot \
     waybar \
@@ -38,4 +38,5 @@ rpm-ostree install \
     polkit-kde-agent
 
 #### Example for enabling a System Unit File
+
 systemctl enable podman.socket
