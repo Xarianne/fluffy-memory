fc41593e7d653e808fe9afbcb8ef648ca8aa55ee
Xarianne <games.clinking002@passinbox.com>
2025-07-02 13:10:09 +0100

    clean up

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,9 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-# dnf5 install -y
+# dnf5 install -y \
+#   app1 \
+#   app2 \
 
 # Use a COPR Example:
 #
