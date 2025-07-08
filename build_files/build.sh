a4ee1e1bf1bf75bcfe3188306e98137fac6488b8
Xariann <games.clinking002@passinbox.com>
2025-07-08 20:46:30 +0100

    hyprland + ml4w

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,10 +4,16 @@ set -ouex pipefail
 
 ### Install packages
 
+# Enable the correct COPR repository for chezmoi
+dnf5 -y copr enable superatomic/chezmoi
+
 # --- Add your Hyprland installation here ---
 
 # Enable solopasha/hyprland COPR for potentially newer versions
 dnf5 -y copr enable solopasha/hyprland
+
+# Create the .gnupg directory for gpg key import
+mkdir -p /root/.gnupg
 
 # Install Hyprland and its development packages, and xdg-desktop-portal-hyprland
 dnf5 -y install hyprland hyprland-devel xdg-desktop-portal-hyprland
@@ -25,6 +31,7 @@ rm -rf /tmp/ml4w-dotfiles
 
 # It's good practice to disable COPRs if you don't want them enabled on the final image
 dnf5 -y copr disable solopasha/hyprland
+dnf5 -y copr disable superatomic/chezmoi
 
 # --- End Hyprland installation ---
 
