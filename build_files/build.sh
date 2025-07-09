4f11300521889106f02f65d5fba26d73d156a7c7
Xariann <games.clinking002@passinbox.com>
2025-07-09 08:21:06 +0100

    reset

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-#dnf5 install -y tmux
+dnf5 install -y tmux
 
 # Use a COPR Example:
 #
@@ -22,6 +22,3 @@ set -ouex pipefail
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
-
-echo "--- Performing required cleanup ---"
-rm -rf /usr/etc
